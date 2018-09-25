f9df32540c41688aa3c2ec70021a0d0c0b298478
cvimard <cvimard@socialobjects.co>
2018-09-25 10:56:29 +0200

    feat(item): add a list item

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import List from './list'
+
 import './index.scss'
 
+const list = [{
+  name: 'Cyril',
+  age: 31
+}, {
+  name: 'Echo',
+  age: 24
+}, {
+  name: 'Guillaume',
+  age: 12
+}]
+
 const App = () => (
-  <div>Hello React!</div>
+  <List list={list} />
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,10 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: [
+          'babel-loader',
+          'eslint-loader'
+        ]
       }
     ]
   },
